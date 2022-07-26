2e30ae018768e2dfc00acac3c909defb4f15f9f0
Rodrigo Greco <rodrigreco10@gmail.com>
2022-07-26 17:02:53 -0300

    Nuevo repo para incorporar arrays

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const totalesCF = []
+
+function calcularMontoDeCF(precio){
+    return precio * 0.21;
+}
+
+let sumatoria = 0;
+let precioProductos
+do{
+    precioProductos = prompt('Si sos Responsable Inscripto ingresa el monto neto de tu factura y te diremos el IVA CF que podes deducir en tu ddjj mensual de IVA');
+    const precioProductosParseado = parseFloat(precioProductos);
+    if(precioProductosParseado){
+        const productoIva = calcularMontoDeCF(precioProductosParseado)
+        sumatoria += productoIva
+        alert('Iva a Computar '+ productoIva)
+    }
+    if(precioProductos != 'Listo'){
+        totalesCF.push(precioProductosParseado)
+    }
+    
+}while(precioProductos != 'Listo')
+alert('El Importe a computar de CF es ' + sumatoria )
+alert('los Importes Netos ingresados son:\n'+ totalesCF.join("\n"))
